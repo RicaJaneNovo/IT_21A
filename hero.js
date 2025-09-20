8a27c5cb6b84004fb14f7b64aaaafba7178a2b60
Rica Jane Novo <ricajanenovo88@gmail.com>
2025-09-19 21:33:16 -0800

    Added item for composition

--- hero.js
+++ hero.js
@@ -5,11 +5,20 @@ class Hero{
         this.name = name;
         this.#health = health;
         this.attack = attack;
+        this.items = [];
     }
 
     getHealth(){
         return this.#health;
     }
+
+     getName(){
+        console.log(this.name);
+    }
+    getAttack(){
+        console.log(this.attack);
+    }
+
      getStats(){
         console.log("\n");
         console.log("Name: " + this.name);
@@ -19,11 +28,20 @@ class Hero{
     }
 }
 
+   addItem(item){
+     this.items.push(item);
+   }
+
+    totalAttack(){
+        return this.attack +this.items.reduce((sum,i)=>sum + i.bonusAttack,0);
+    }
+
+
 class Warrior extends Hero{
     useAbility(){
         console.log(`${this.name} uses power Strike`);
     }
-}
+}    
 
 class Mage extends Hero{
     constructor(name,health,attack,mana){
@@ -34,19 +52,42 @@ class Mage extends Hero{
         console.log(`${this.name} casts Fireball!`);
     }
 }
-const warrior = new Warrior("Thorin",100,10);
-warrior.useAbility();
 
-const mage = new Mage("Gandalf",80,8,50);
-mage.useAbility();
+class Item{
+    constructor(name,bonusAttack){
+        this.name = name;
+        this.bonusAttack = bonusAttack;
+    }
+}
+
+const sword = new Item("Sword",5);
+const staff = new Item("Staff", 3);
+
 
 const Thorin = new Warrior("Thorin",100,10);
-//Thorin.useAbility();
-Thorin.getStats();
+warrior.useAbility();
 
 const Gandalf = new Mage("Gandalf",80,8,50);
+mage.useAbility();
+
+const warrior = new Warrior("Thorin",100,10);
+//Thorin.useAbility();
+Thorin.getStats();
+Thorin.getName();
+Thorin.getAttack();
+
+Thorin.addItem(sword);
+console.log(Thorin.totalAttack());
+Thorin.addItem(sword);
+console.log(Thorin.totalAttack());
+Thorin.addItem(sword);
+console.log(Thorin.totalAttack());
+Thorin.addItem(sword);
+console.log(Thorin.totalAttack());
+
+//const mage = new Mage("Gandalf",80,8,50);
 //Gandalf.useAbility();
-Gandalf.getStats();
+//Gandalf.getStats();
 
 
 function performAbility(hero){
@@ -56,3 +97,5 @@ function performAbility(hero){
 
 performAbility(Thorin);
 performAbility(Gandalf);
+//performAbility(Thorin);
+//performAbility(Gandalf);
